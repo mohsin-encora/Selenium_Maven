65ba6feb7bba7a31cd36708c2d6e8a1b129a2df2
mohsin-encora <mohsin.khan@encora.com>
2022-09-05 13:12:43 +0530

    Selenium maven

---
+++ src/test/java/testcases/SimpliLearnLoginTest.java
@@ -0,0 +1,11 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpliLearnLoginTest {
+	
+	WebDriver wd = new ChromeDriver();
+	
+
+}
