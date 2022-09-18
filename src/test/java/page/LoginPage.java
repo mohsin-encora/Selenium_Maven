986e62f957be6590d08361e8daba32a0c6b8911f
mohsin-encora <mohsin.khan@encora.com>
2022-09-18 12:13:53 +0530

    Pushing Updated Changes

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -6,36 +6,38 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-
 public class LoginPage {
-	
-	@FindBy(linkText="Log in")
+
+	@FindBy(linkText = "Log in")
 	WebElement LoginLink;
-	
-	@FindBy(name="user_login")
+
+	@FindBy(name = "user_login")
 	WebElement UserName;
-	
-	@FindBy(id="password")
+
+	@FindBy(id = "password")
 	WebElement password;
-	
-	@FindBy(className="rememberMe")
+
+	@FindBy(className = "rememberMe")
 	WebElement checkbox;
-	
-	@FindBy(name="btn_login")
+
+	@FindBy(name = "btn_login")
 	WebElement LoginBtn;
+
 	
-    
+
 	WebDriver driver;
-	// Page factory it willl support @Find By - you need to mention your driver and class name 
+
+	// Page factory it willl support @Find By - you need to mention your driver and
+	// class name
 	public LoginPage(WebDriver basedriver) {
 		this.driver = basedriver;
 		PageFactory.initElements(basedriver, this);
 	}
 
-	public void Login(String Username , String Password) {
+	public void Login(String Username, String Password) {
 
 		// Step1. Click on Login Link
-	
+
 		LoginLink.click();
 
 		// Step 2 Enter Username
@@ -43,18 +45,19 @@ public class LoginPage {
 		UserName.sendKeys(Username);
 
 		// Step 3 Enter Password
-	
+
 		password.sendKeys(Password);
 
 		// Step 4 Click remember me checkbox
-	
+
 		checkbox.click();
 
 		// Step 5 Click on Login Button
-		
+
 		LoginBtn.click();
 
 	}
 
-	
+
+
 }
--- src/test/java/testcases/SimpliLearnLoginTest.java
+++ src/test/java/testcases/SimpliLearnLoginTest.java
@@ -5,16 +5,19 @@ package testcases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
 
 import page.LoginPage;
 
 public class SimpliLearnLoginTest extends BaseClass {
-	
+	LoginPage loginPage;
 
 	@Test(priority = 1)
 	public void Test1() {
-
+          test.log(LogStatus.INFO, "Test1 Execution Started");
 		LoginPage Loginpage = new LoginPage(driver);
 		Loginpage.Login("abc@xyz.com", "Test@123");
 		// Step6: Validate the error message on screen
@@ -29,16 +32,33 @@ public class SimpliLearnLoginTest extends BaseClass {
 		 * if (ActError.equals(ExpError)) { System.out.println("TC PAssed"); } else {
 		 * System.out.println("Tc Failed"); }
 		 */
+		//driver.findElements(By.xpath("").)
 	
 		
 	}
 	
 	
 	@Test(priority = 2)
-	public void Test2() {
+	@Parameters({"uname","pwd"})
+	public void Test2(String UserName, String Password) {
+		 test.log(LogStatus.INFO, "Test2 Execution Started");
 		LoginPage Loginpage = new LoginPage(driver);
-		Loginpage.Login("qa.mohsink@gmail.com","IlsaSimmi@1");
+		Loginpage.Login(UserName,Password);
 		
 	}
+	
+	@Test
+	public void Test3() {
+		
+		 test.log(LogStatus.INFO, "Test3 Execution Started");
+		
+		String UserName = sheet.getRow(1).getCell(0).getStringCellValue();
+		System.out.println(UserName);
+		String Password = sheet.getRow(1).getCell(1).getStringCellValue();
+		
+		LoginPage lp = new LoginPage(driver);
+		lp.Login(UserName,Password);
+	}
+
 
 }
